2aa41fd918f0696f70e1a52d25ddd566fd8f7f1a
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2014-02-24 09:37:25 +0100

    creato progetto frontend

---
+++ FrontEnd/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+
+[assembly: OwinStartup(typeof(FrontEnd.Startup))]
+
+namespace FrontEnd
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
