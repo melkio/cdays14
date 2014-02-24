519c099c3798b7a82efd36c048042eed47abf466
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2014-02-24 09:28:45 +0100

    prima demo introduzione: chat

---
+++ SignalR.Intro/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+
+namespace SignalR.Intro
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        public void BroadcastMessage(String message)
+        {
+            Clients.All.showMessage(message);
+        }
+    }
+}
